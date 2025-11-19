d9fa6daff123437ce2235e027245ccdadd11f900
Dark-Ghost1 <wazrahm@gmail.com>
2025-11-19 14:48:32 +0300

    feat: Implement base _printf function
    This initial commit introduces a custom implementation of the printf function.
    
    It includes the core logic for parsing format strings and handles the following specifiers: c, s, %, i, d.

---
+++ _printf.c
@@ -0,0 +1,55 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * _printf - Produces output according to a format string.
+ * @format: The character string that specifies the format of the output.
+ *
+ * Description: This function prints a formatted string to the standard output.
+ * It handles format specifiers like %c, %s, %%, %d, and %i.
+ * Return: The total number of characters printed to the output.
+ */
+int _printf(const char *format, ...)
+{
+	int i = 0, j = 0, len = 0, last_print = 0;
+	va_list args;
+	prt_f prt_fun[] = {
+		{'c', print_char},
+		{'s', print_str},
+		{'%', print_per},
+		{'i', print_int},
+		{'d', print_int},
+		{'\0', NULL}
+	};
+	va_start(args, format);
+
+	while (format[i] != '\0')
+	{
+		j = 0;
+		if (format[i] == '%')
+		{
+			i++;
+			while (prt_fun[j].c != '\0')
+			{
+				if (format[i] == prt_fun[j].c)
+				{
+					len += write(1, &format[last_print], i - last_print - 1);
+					len += prt_fun[j].func_ptr(&args);
+					last_print = i + 1;
+					break;
+				}
+				j++;
+				if (!prt_fun[j].c)
+					i++;
+			}
+		}
+		else
+			i++;
+	}
+	if (i != last_print)
+		len += (write(1, &format[last_print], i - last_print));
+	va_end(args);
+	return (len);
+}
