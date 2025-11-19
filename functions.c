3088dd984a6e422fe1c5db6c9d8f777d74118cc7
badriahalmalki <badryahbalmalki@gmail.com>
2025-11-19 18:19:58 +0300

    editing print_str function to handle null value

--- functions.c
+++ functions.c
@@ -3,10 +3,10 @@
 #include "main.h"
 
 /**
- * print_char - prints a character
- * @args: va_list containing the character to print
- * Return: number of characters printed
- */
+* print_char - prints a character
+* @args: va_list containing the character to print
+* Return: number of characters printed
+*/
 int print_char(va_list *args)
 {
 	char c = (char)(va_arg(*args, int));
@@ -14,25 +14,28 @@ int print_char(va_list *args)
 	return (write(1, &c, 1));
 }
 /**
- * print_str - prints a string
- * @args: va_list containing the string to print
- * Return: number of characters printed
- */
+* print_str - prints a string
+* @args: va_list containing the string to print
+* Return: number of characters printed
+*/
 int print_str(va_list *args)
 {
 	int i;
+
 	char *s = va_arg(*args, char *);
 
+	if (s == NULL)
+		s = "(null)";
 	for (i = 0; s[i] != '\0'; i++)
 		;
 
 	return (write(1, s, i));
 }
 /**
- * print_per - prints a percent sign
- * @args: va_list (not used)
- * Return: number of characters printed
- */
+* print_per - prints a percent sign
+* @args: va_list (not used)
+* Return: number of characters printed
+*/
 int print_per(va_list *args)
 {
 	char p = '%';
@@ -43,14 +46,16 @@ int print_per(va_list *args)
 }
 
 /**
- * print_int - prints an integer
- * @args: va_list containing the integer to print
- * Return: number of characters printed
- */
+* print_int - prints an integer
+* @args: va_list containing the integer to print
+* Return: number of characters printed
+*/
 int print_int(va_list *args)
 {
 	long int n;
+
 	unsigned long int tmp1, tmp2 = 1, len = 0;
+
 	char nump;
 
 	n = va_arg(*args, int);
